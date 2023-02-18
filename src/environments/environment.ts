c813de1f22ef083e689e4775d4c1c26b6a622279
Ed O'Connor <ed@oconnor.app>
2023-02-18 10:13:23 -0500

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAGmyumVCgyGNq4xv0jyL2YoICBNTbYQsg',
+    authDomain: 'project.firebaseapp.com',
+    databaseURL: 'https://project.firebaseio.com',
+    projectId: 'ng-cms-d58a8',
+    storageBucket: 'project.appspot.com',
+    messagingSenderId: '...',
+  },
+};
